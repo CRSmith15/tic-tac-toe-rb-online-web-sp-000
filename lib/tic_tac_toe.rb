f9c51f29f886a3c1270016ca39587941a5a614ce
Chance Smith <chancesmith97@hotmail.com>
2020-02-29 21:36:47 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -67,7 +67,7 @@ def turn(board)
     input = gets.chomp
     index = input_to_index(input)
     if valid_move?(board, index)
-      move(board, index, token )
+      move(board, index, token= 'X' )
       display_board(board)
     else
       turn(board)
